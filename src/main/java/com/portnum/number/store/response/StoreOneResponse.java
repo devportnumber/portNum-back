5543d26565a10f9a5ca2a2fde3a7140cdae72d68
ByungSun Song <46879319+SBSun@users.noreply.github.com>
2024-04-02 00:22:30 +0900

    fix: 팝업 단일 조회 - 반환 형식 수정 (#6)

--- src/main/java/com/portnum/number/store/response/StoreOneResponse.java
+++ src/main/java/com/portnum/number/store/response/StoreOneResponse.java
@@ -18,21 +18,33 @@ import lombok.ToString;
 public class StoreOneResponse {
 
   /**
-   * 팝업 이름
+   * 팝업 Id
    */
-  private String name;
-  /**
-   * 팝업 키워드
-   */
-  private List<String> keywords;
-  /**
-   * 팝업 지역
-   */
-  private String neighborhood;
+  private Integer id;
   /**
    * 팝업 카테고리
    */
   private String category;
+  /**
+   * 영업 기간
+   */
+  private String dates;
+  /**
+   * 영업 시간
+   */
+  private String time;
+  /**
+   * 팝업 이름
+   */
+  private String name;
+  /**
+   * 경도
+   */
+  private String longitude;
+  /**
+   * 위도
+   */
+  private String latitude;
   /**
    * 주소
    */
@@ -40,36 +52,43 @@ public class StoreOneResponse {
   /**
    * 상세 주소
    */
-  private String addressDetail;
+  private String address_detail;
   /**
-   * 팝업 이미지
+   * 팝업 지역
    */
-  private List<String> images;
+  private String neighborhood;
+  /**
+   * 팝업 키워드
+   */
+  private List<String> keywords;
   /**
    * 설명
    */
   private String description;
   /**
-   * 영업 기간
+   * 팝업 이미지
    */
-  private String businessPeriod;
+  private List<String> images;
   /**
-   * 영업 시간
+   * 등록일
    */
-  private String businessHours;
+  private String regdt;
 
   public StoreOneResponse(Store store) {
-    this.name = store.getName();
-    this.keywords = Arrays.stream(store.getKeywords().split(",")).toList();
-    this.neighborhood = store.getNeighborhood();
+    this.id = store.getStoreId();
     this.category = store.getCategory();
+    this.dates = store.getStartDate() + " - " + store.getEndDate();
+    this.time = DateUtils.getDayOfWeek(LocalDate.now()) + " " + store.getStartTime() + " ~ "
+        + store.getEndTime();
+    this.name = store.getName();
+    this.longitude = store.getLongitude();
+    this.latitude = store.getLatitude();
     this.address = store.getAddress();
-    this.addressDetail = store.getAddressDetail();
-    this.images = Arrays.stream(store.getImages().split(",")).toList();
+    this.address_detail = store.getAddressDetail();
+    this.neighborhood = store.getNeighborhood();
+    this.keywords = Arrays.stream(store.getKeywords().split(",")).toList();
     this.description = store.getDescription();
-    this.businessPeriod = store.getStartDate() + " - " + store.getEndDate();
-    this.businessHours =
-        DateUtils.getDayOfWeek(LocalDate.now()) + " " + store.getStartTime() + " ~ "
-            + store.getEndTime();
+    this.images = Arrays.stream(store.getImages().split(",")).toList();
+    this.regdt = store.getRegDt().toLocalDate().toString();
   }
 }
