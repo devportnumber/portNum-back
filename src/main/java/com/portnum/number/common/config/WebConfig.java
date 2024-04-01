4937c5cf71f9154486793a9fa64224fed08b44f7
SBSun <46879319+SBSun@users.noreply.github.com>
2024-04-01 22:47:01 +0900

    fix: cors 설정

---
+++ src/main/java/com/portnum/number/common/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.portnum.number.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * CORS 설정
+ *
+ * @author 송병선
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOriginPatterns("*")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+        .allowCredentials(true);
+  }
+}
