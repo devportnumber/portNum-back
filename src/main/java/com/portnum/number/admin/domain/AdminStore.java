4bd2a2cd2cf45f5449235cf2573184b72cac0106
juile720 <75417503+juile720@users.noreply.github.com>
2024-06-25 18:48:57 +0900

    20240625 admin (#13)
    Co-authored-by: jieunan <jieun.an@ibm.com>

---
+++ src/main/java/com/portnum/number/admin/domain/AdminStore.java
@@ -0,0 +1,44 @@
+package com.portnum.number.admin.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners({AuditingEntityListener.class})
+@Table(name = "store")
+public class AdminStore {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer storeId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "category", nullable = false)
+    private String category;
+
+    @Column(name = "start_date", nullable = false)
+    private String startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private String endDate;
+
+    @Column(name = "stat", nullable = true)
+    private String stat;
+
+    public AdminStore(Integer storeId, String name, String category, String startDate, String endDate, String stat) {
+        this.storeId = storeId;
+        this.name = name;
+        this.category = category;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.stat = stat;
+    }
+}
