1b56dfc1a3db43d4ba44ac799243cd2cf489c34e
HanYoonSoo <hdbstn3055@gmail.com>
2024-09-27 23:21:00 +0900

    refactor: UrlUtils 수정

--- src/main/java/com/portnum/number/global/utils/UrlUtils.java
+++ src/main/java/com/portnum/number/global/utils/UrlUtils.java
@@ -6,12 +6,12 @@ public class UrlUtils {
     public static final List<String> EXCLUDE_URLS = List.of(
             "/auth/login", "/docs/index.html",
             "/admin/signup", "/admin/valid", "/admin/lost", "/admin/health",
-            "/admin/image", "/admin/reissue", "/admin/popup/api"
+            "/admin/image", "/auth/reissue", "/admin/popup/api"
     );
 
     public static final List<String> PERMITTED_URLS = List.of(
             "/admin/signup", "/admin/health", "/admin/popup/api/**",
-            "/admin/valid/**", "/admin/lost/**", "/admin/image", "/admin/reissue"
+            "/admin/valid/**", "/admin/lost/**", "/admin/image", "/auth/reissue"
     );
 }
 
