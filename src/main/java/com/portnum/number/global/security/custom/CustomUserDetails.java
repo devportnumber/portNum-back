7e74608433f1459500cbd16c515368a71ca15479
HanYoonSoo <hdbstn3055@gmail.com>
2024-11-03 22:04:15 +0900

    feat: 로그아웃 필터 추가 및 토큰 공통 예외처리

--- src/main/java/com/portnum/number/global/security/custom/CustomUserDetails.java
+++ src/main/java/com/portnum/number/global/security/custom/CustomUserDetails.java
@@ -37,13 +37,13 @@ public class CustomUserDetails extends Admin implements UserDetails {
         this.urlName = admin.getUrlName();
     }
 
-    private CustomUserDetails(String email, RoleType role){
-        this.email = email;
+    private CustomUserDetails(String loginId, RoleType role){
+        this.loginId = loginId;
         this.roleType = role;
     }
 
-    private CustomUserDetails(String email, RoleType role, String password) {
-        this.email = email;
+    private CustomUserDetails(String loginId, RoleType role, String password) {
+        this.loginId = loginId;
         this.roleType = role;
         this.password = password;
     }
@@ -52,8 +52,8 @@ public class CustomUserDetails extends Admin implements UserDetails {
         return new CustomUserDetails(admin);
     }
 
-    public static CustomUserDetails of(String email, RoleType role){
-        return new CustomUserDetails(email, role);
+    public static CustomUserDetails of(String loginId, RoleType role){
+        return new CustomUserDetails(loginId, role);
     }
 
     public static CustomUserDetails of(String email, RoleType role, String password) {
--- src/main/java/com/portnum/number/global/security/custom/LoginSuccessHandler.java
+++ src/main/java/com/portnum/number/global/security/custom/LoginSuccessHandler.java
@@ -8,22 +8,28 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 
+    private final ObjectMapper objectMapper;
+
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        FilterChain chain, Authentication authentication) throws IOException, ServletException {
         AuthenticationSuccessHandler.super.onAuthenticationSuccess(request, response, chain, authentication);
     }
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
         CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-        LoginResponse loginResponse = new LoginResponse(customUserDetails.getId(), customUserDetails.getLoginId(), customUserDetails.getEmail(), customUserDetails.getNickName(), customUserDetails.getProfileUrl(), customUserDetails.getIsRqPwChange(), customUserDetails.getUrlName());
+        LoginResponse loginResponse = LoginResponse.from(customUserDetails);
         response.setContentType("application/json;charset=UTF-8");
         response.getWriter().write(converObjectToJson(DataResponseDto.of(loginResponse)));
     }
@@ -32,7 +38,6 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
         if (object == null) {
             return null;
         }
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.writeValueAsString(object);
+        return objectMapper.writeValueAsString(object);
     }
 }
